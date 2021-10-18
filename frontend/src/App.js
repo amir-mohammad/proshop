6fd0aeb0e296f328a6bb54474da7b4383ba9d73d
devtricks-company <amir.mo.azarbashi@gmail.com>
2021-10-18 17:40:24 +0330

    setup and inital react

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+// use className insted of class
+// use htmlFor insted of for in label
+// <img src="" /> <input />
+// return only one parent
+function App() {
+  return <h1>Welcome to Proshop</h1>;
+}
+
+export default App;
