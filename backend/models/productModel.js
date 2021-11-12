36a7ad8f2bdd925b0c1f99f3b85852eb24705388
amir-mohammad <amirm.azarbashi@gmail.com>
2021-11-12 19:32:16 +0330

    fixing product description

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -24,6 +24,10 @@ const productSchema = mongoose.Schema({
         type:String,
         required:true
     },
+    description:{
+        type:String,
+        required:true
+    },
     category:{
         type:String,
         required:true
